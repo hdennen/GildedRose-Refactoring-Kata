983ccf2f55bb95e35c1292a1375559a981e7851c
hdennen <h@harrydennen.com>
2017-07-09 23:33:39 +0200

    use item handler function object

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -17,21 +17,74 @@ export class GildedRose {
     static NORMAL_DEGRADATION = 1;
     static CONJURED_DEGRADATION = GildedRose.NORMAL_DEGRADATION * 2;
     items: Array<Item>;
+    handlers: any;
 
     constructor(items = []) {
         this.items = items;
+        this.handlers = this.createItemHandlers();
     }
     
-    isBrie(item): boolean {
-        return item.name === 'Aged Brie';
-    }
+    createItemHandlers() {
+        let obj = {};
+        
+        obj['default'] = (item) => {
+            item.sellIn--;
+            if (item.quality === GildedRose.MIN_QUALITY) return item;
+            item.quality--;
     
-    isBackStagePass(item): boolean {
-        return item.name === 'Backstage passes to a TAFKAL80ETC concert';
-    }
+            if (item.sellIn < 0 && item.quality !== GildedRose.MIN_QUALITY) {
+                item.quality--;
+            }
     
-    isSulfuras(item): boolean {
-        return item.name === 'Sulfuras, Hand of Ragnaros';
+            return item;
+        }
+        
+        obj['Aged Brie'] = (item) => {
+            item.sellIn--;
+            if (this.isQualityMax(item)) return item;
+            item.quality++;
+            return item;
+        }
+        
+        obj['Sulfuras, Hand of Ragnaros'] = (item) => {
+            return item;
+        }
+        
+        obj['Backstage passes to a TAFKAL80ETC concert'] = (item) => {
+            item.sellIn--;
+            if (item.sellIn < 1) {
+                item.quality = GildedRose.MIN_QUALITY;
+                return item;
+            }
+            item.quality++;
+    
+            if (item.quality === GildedRose.MAX_QUALITY) return item;
+    
+            if (this.isWithinFiveDays(item)) {
+                item.quality++;
+                if (this.isQualityMax(item)) return item;
+                item.quality++;
+                return item;
+            }
+    
+            if (this.isWithinTenDays(item)) {
+                item.quality++;
+                return item;
+            }
+        }
+        
+        obj['Conjured'] = (item) => {
+            item.sellIn--;
+            item.quality -= GildedRose.CONJURED_DEGRADATION;
+    
+            if (item.sellIn < 0) item.quality -= GildedRose.CONJURED_DEGRADATION;
+    
+            if (item.quality < 0) item.quality = 0;
+    
+            return item;
+        }
+        
+        return obj;
     }
     
     isWithinFiveDays(item): boolean {
@@ -45,69 +98,14 @@ export class GildedRose {
     isQualityMax(item): boolean {
         return item.quality === GildedRose.MAX_QUALITY;
     }
-    
-    isNoQuality(item): boolean {
-        return item.quality === GildedRose.MIN_QUALITY;
-    }
-    
-    isConjured(item): boolean {
-        return item.name === 'Conjured';
-    }
 
     updateQuality(): Array<Item> {
         return this.items.map(item => {
-            if (this.isSulfuras(item)) return item;
-            item.sellIn--;
-            
-            if (this.isQualityMax(item) || (this.isNoQuality(item) && !this.isBrie(item))) {
-                return item;
+            if (this.handlers.hasOwnProperty(item.name)) {
+                return this.handlers[item.name](item);
             }
             
-            if (this.isBrie(item)) {
-                item.quality++;
-                return item;
-            }
-            
-            if (this.isBackStagePass(item)) {
-                if (item.sellIn < 1) {
-                    item.quality = 0;
-                    return item;
-                }
-                item.quality++;
-                
-                if (item.quality === 50) return item;
-                
-                if (this.isWithinFiveDays(item)) {
-                    item.quality++;
-                    if (this.isQualityMax(item)) return item;
-                    item.quality++;
-                    return item;
-                }
-                
-                if (this.isWithinTenDays(item)) {
-                    item.quality++;
-                    return item;
-                }
-            }
-            
-            if (this.isConjured(item)) {
-                item.quality -= GildedRose.CONJURED_DEGRADATION;
-                
-                if (item.sellIn < 0) item.quality -= GildedRose.CONJURED_DEGRADATION;
-                
-                if (item.quality < 0) item.quality = 0;
-                
-                return item;
-                
-            }
-            
-            item.quality--;
-            
-            if (item.sellIn < 0 && item.quality !== 0) {
-                item.quality--;
-            }
-            
-            return item;
+            return this.handlers.default(item);
         });
     }
 }
